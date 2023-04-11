ba63d09ff0203d30520f6e050f6ef9d0395d359c
Arkadii Vodolazskyi <49953024+ArkadiiVodolazskyi@users.noreply.github.com>
2023-04-11 23:57:56 +0300

    Added updated dist folder.

--- src/main.js
+++
@@ -1,6 +0,0 @@
-import { createApp } from 'vue';
-import App from './App.vue';
-
-import './assets/main.sass';
-
-createApp(App).mount('#app');
--- src/services/LanguageService.js
+++
@@ -1,9 +0,0 @@
-export default class LanguageService {
-  constructor() {
-    this.browserLanguage = navigator.language;
-  }
-
-  get language() {
-    return this.browserLanguage.slice(0, 2);
-  }
-}
